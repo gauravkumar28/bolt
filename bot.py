e1bb5983fd497fbee752ded2ae70e6b4afbcef02
Gaurav Kumar3 <gaurav.kumar3@C02QD5TFG8WP.local>
2017-01-27 03:23:10 +0530

    no mongo on heroku

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask import request, json
-
+import os
 
 app = Flask(__name__)
 
